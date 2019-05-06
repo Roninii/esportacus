2a883e5316a472c82a128e6dffbdff8ec7d6197d
Ronnie <villarini91@gmail.com>
2019-05-05 22:01:37 -0500

    add netlify lambda to get leagues

---
+++ src/lambda/getleagues.js
@@ -0,0 +1,40 @@
+const axios = require(`axios`);
+
+exports.handler = (event, context, callback) => {
+  // const { API_URL, TOKEN } = process.env;
+  const API_URL = `https://api.pandascore.co`;
+  const TOKEN = `Svkm0PUE2PwoSGBOjwKz3dxEb1TLfnWexGZRFCO1F2pmkdSHmNU`;
+
+  const QUERY = `/leagues`;
+  const URL = `${API_URL}${QUERY}?token=${TOKEN}`;
+
+  // send response
+  const send = body => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(body),
+    });
+  };
+
+  // perform api call
+  const getLeagues = () => {
+    axios
+      .get(URL)
+      .then(res => {
+        const body = {
+          "current-page": res.headers[`x-page`],
+          "total-results": res.headers[`x-total`],
+          "per-page": res.headers[`x-per-page`],
+          data: res.data,
+        };
+
+        send(body);
+      })
+      .catch(err => send(err));
+  };
+
+  // make sure method is GET
+  if (event.httpMethod === `GET`) {
+    getLeagues();
+  }
+};
